7d19332bf3f0aeb52e08bd6a86a84009d9bc6c3a
Griffinbro <ronethsandeepa@gmail.com>
2025-09-25 03:43:37 +0530

    Added footer to common layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -43,6 +43,43 @@ const Layout = (): JSX.Element => {
         <Outlet />
       </main>
 
+      <footer className="footer footer-horizontal">
+        <nav>
+          <h6 className="footer-title">Services</h6>
+          <a className="link link-hover">Help</a>
+          <a className="link link-hover">Official Blog</a>
+          <a className="link link-hover">Advertisement</a>
+          <a className="link link-hover">VIP Membership</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Company</h6>
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Careers</a>
+          <a className="link link-hover">Press</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Legal</h6>
+          <a className="link link-hover">Terms of use</a>
+          <a className="link link-hover">Privacy policy</a>
+          <a className="link link-hover">Cookie policy</a>
+        </nav>
+        <form>
+          <h6 className="footer-title">Newsletter</h6>
+          <fieldset style={{ width: "20rem", maxWidth: "100%" }}>
+            <label>Enter your email address</label>
+            <div className="join">
+              <input
+                type="text"
+                placeholder="username@site.com"
+                className="input input-bordered join-item"
+              />
+              <button className="btn btn-primary join-item">Subscribe</button>
+            </div>
+          </fieldset>
+        </form>
+      </footer>
+
     </>
   );
 }
