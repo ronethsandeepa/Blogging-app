9e7f0444d992a5cc3637ca874769528e556f2ff9
Griffinbro <ronethsandeepa@gmail.com>
2025-09-25 03:15:21 +0530

    Designed HomePage UI template

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,14 +1,23 @@
 import type { JSX } from "react";
+import image1 from "../assets/img3.png";
 
 const HomePage = (): JSX.Element => {
     return (
         <>
-            <div className="hp-hero">
-                <h1 className="hp-headline">
-                    Turn your <span className="hp-gradient">Experience</span> into <span className="hp-gradient">Impact</span>
-                </h1>
-                <p className="hp-subtext">publish your ideas and knowledge that help others learn - build your reputation</p>
-            </div>
+            <section className="hero-wrapper">
+                <div className="hp-hero">
+                    <h1 className="hp-headline">
+                        Turn your <span className="hp-gradient">Experience</span> into <span className="hp-gradient">Impact</span>
+                    </h1>
+                    <p className="hp-subtext">publish your ideas and knowledge that help others learn - build your reputation</p>
+                </div>
+
+                <img
+                    src={image1}
+                    alt="hero"
+                    className="hp-hero-img"
+                />
+            </section>
 
         </>
     )
